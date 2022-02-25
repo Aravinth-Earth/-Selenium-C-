6a4c711bc6b1b779e3dafe113c9828b19e1590eb
Aravinth <55803552+Aravinth-Earth@users.noreply.github.com>
2022-02-26 03:47:43 +0530

    added all as a local backup

---
+++ Selenium-Csharp-UnitTest/Selenium-Csharp-UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium-Csharp-UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium-Csharp-UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97cab26b-a726-44ec-b490-c4e02c1486fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
